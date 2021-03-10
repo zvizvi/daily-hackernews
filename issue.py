ed807b1e1e1b286d0c79439eddd8529f2f68962b
zvizvi <5633910@gmail.com>
2021-03-10 18:21:25 +0200

    Update issue.py

--- issue.py
+++ issue.py
@@ -5,7 +5,7 @@ import requests
 
 
 def create_issue(title, body, milestone=None, labels=None, assignees=None):
-    url = 'https://api.github.com/repos/xueyuanl/daily-hackernews/issues'
+    url = 'https://api.github.com/repos/zvizvi/daily-hackernews/issues'
 
     headers = {'Content-Type': 'application/json', 'Authorization': 'token ' + os.getenv('ACCESS_TOKEN')}
     body = {'title': title, 'body': body}
